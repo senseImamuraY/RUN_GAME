cd831b403c4621acff804e9f3a6675840482302e
senseImamuraY <89624108+senseImamuraY@users.noreply.github.com>
2023-06-11 18:20:25 +0900

    added skybox
    背景やイメージなどを追加

--- Assets/Scripts/Environment/Goal.cs
+++ Assets/Scripts/Environment/Goal.cs
@@ -11,13 +11,19 @@ public class Goal : MonoBehaviour
     [SerializeField]
     List<GameObject> confettiList;
 
+    AudioSource audioSource;
+
+    [SerializeField]
+    AudioClip cracker, prise;
+
     void Start()
     {
         centerPos = transform.position;
         //centerPos = new Vector3(transform.position.x, 0, transform.position.z);
         goal = GetComponent<CustomCubeCollider>();
         goal.SetCenter(centerPos);
-        Debug.Log(centerPos);
+        //Debug.Log(centerPos);
+        audioSource = GetComponent<AudioSource>();
     }
 
     //private void OnTriggerEnter(Collider other)
@@ -38,6 +44,21 @@ public class Goal : MonoBehaviour
             }
             player.Clear(centerPos + new Vector3(0, transform.localScale.y /2, 0));
             Debug.Log("clear!");
+
+            Invoke("PlayCracker", 0.2f);
+            Invoke("PlayPrise", 1f);
         }
+    }
+
+    private void PlayCracker()
+    {
+        audioSource.clip = cracker;
+        audioSource.Play();
+    }
+
+    private void PlayPrise()
+    {
+        audioSource.clip = prise;
+        audioSource.Play();
     }
 }
